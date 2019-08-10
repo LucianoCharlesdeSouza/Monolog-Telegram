8bf4219bf5b2e1ace64372fa34efd9d2120203b8
Luciano Charles de Souza <souzacomprog@gmail.com>
2019-08-09 23:25:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . 'Config/config.php');
+require_once(__DIR__ . '/Config/config.php');
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
